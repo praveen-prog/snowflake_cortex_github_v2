9b3ee8f777a85b4680d79e80d672a536d4d3eae6
Praveen Kumar Chandran <praveenhome@Praveens-MacBook-Pro.local>
2024-12-27 11:08:43 -0500

    tru lens dashboard added

--- src/cortex_search_retriever.py
+++ src/cortex_search_retriever.py
@@ -173,7 +173,7 @@ class CortexSearchRetriever:
             print(f" Leadeboard is  : {tru_session.get_leaderboard()}")
             
             #tru_session.reset_database()   
-            tru_session.run_dashboard(port=8502)   
+            tru_session.run_dashboard(address='https://dev-chat-bot-v2.streamlit.app/dashboard')   
                  
             return result
         except Exception as e:
